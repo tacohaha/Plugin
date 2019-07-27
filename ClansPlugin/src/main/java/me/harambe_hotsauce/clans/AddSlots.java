9225aa205ddfe6cf29ea22d8cf7f9987ca5e0355
Adam Naylor <tacohaha0325@gmail.com>
2019-07-27 00:09:17 -0400

    Added AddSlots command

--- ClansPlugin/src/main/java/me/harambe_hotsauce/clans/AddSlots.java
+++ ClansPlugin/src/main/java/me/harambe_hotsauce/clans/AddSlots.java
@@ -1,8 +1,41 @@
 package me.harambe_hotsauce.clans;
 
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.io.IOException;
+
+import static me.harambe_hotsauce.clans.GenerateFile.getFilePath;
+
 public class AddSlots {
 
-    public AddSlots(String clan) {
+    File file = new File(getFilePath());
+    YamlConfiguration yamlConfiguration = YamlConfiguration.loadConfiguration(file);
 
+    public AddSlots(Player player) {
+        String clan = getClan(player);
+        if (getPermission(player.getName())) {
+            int prevLimit = (int) yamlConfiguration.get("clans." + clan + ".limit");
+            int newLimit = prevLimit + 5;
+            yamlConfiguration.set("clans." + clan + ".limit", newLimit);
+            try {
+                yamlConfiguration.save(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getClan(Player player) {
+        return (String) yamlConfiguration.get("players." + player.getName() + ".clan");
+    }
+
+    public boolean getPermission(String name) {
+        if (yamlConfiguration.get("players." + name + ".Player_Permissions").equals("LEADER")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- ClansPlugin/src/main/java/me/harambe_hotsauce/clans/ClanCommand.java
+++ ClansPlugin/src/main/java/me/harambe_hotsauce/clans/ClanCommand.java
@@ -19,26 +19,29 @@ public class ClanCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args[0].equalsIgnoreCase("create")) {
+        if (args[0].equalsIgnoreCase("Create") | args[0].equalsIgnoreCase("create")) {
             new CreateClan(args[1], (Player) sender);
             return true;
-        } else if (args[0].equalsIgnoreCase("delete")) {
+        } else if (args[0].equalsIgnoreCase("Delete") |  args[0].equalsIgnoreCase("delete")) {
             new DeleteClan((Player) sender);
             return true;
-        } else if (args[0].equalsIgnoreCase("invite")) {
+        } else if (args[0].equalsIgnoreCase("Invite") | args[0].equalsIgnoreCase("invite")) {
             new InvitePlayer(args[1], (Player) sender);
             return true;
-        } else if (args[0].equalsIgnoreCase("leave")) {
+        } else if (args[0].equalsIgnoreCase("Leave") | args[0].equalsIgnoreCase("leave")) {
             new LeaveClan((Player) sender);
             return true;
-        } else if (args[0].equalsIgnoreCase("accept")) {
+        } else if (args[0].equalsIgnoreCase("Accept") | args[0].equalsIgnoreCase("accept")) {
             new AcceptInvite((Player) sender);
             return true;
-        } else if (args[0].equalsIgnoreCase("deny")) {
+        } else if (args[0].equalsIgnoreCase("Deny") | args[0].equalsIgnoreCase("deny")) {
             new DenyInvite((Player) sender);
             return true;
-        } else if (args[0].equalsIgnoreCase("list")) {
+        } else if (args[0].equalsIgnoreCase("List") | args[0].equalsIgnoreCase("list")) {
             new ClanList((Player) sender);
+            return true;
+        } else if (args[0].equalsIgnoreCase("AddSlots") | args[0].equalsIgnoreCase("addSlots") | args[0].equalsIgnoreCase("addslots") | args[0].equalsIgnoreCase("Addslots")) {
+            new AddSlots((Player) sender);
             return true;
         }
         return false;
